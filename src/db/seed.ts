12ff124dbc534d296f55decc1992bad689d17460
Joice <815610763@qq.com>
2026-07-16 17:18:35 +0800

    Upload src\db\seed.ts

---
+++ src/db/seed.ts
@@ -0,0 +1,16 @@
+﻿import { db } from './index'
+import { seedMembers } from './seedMembers'
+import { seedRecipes } from './seedRecipes'
+import { seedChores } from './seedChores'
+
+export async function initSeedData() {
+  const count = await db.members.count()
+  if (count > 0) return
+  await db.transaction('rw', db.members, db.settings, db.recipes, db.choreDefinitions, async () => {
+    await db.members.bulkAdd(seedMembers)
+    await db.recipes.bulkAdd(seedRecipes)
+    await db.choreDefinitions.bulkAdd(seedChores)
+    await db.settings.put({ key: 'seedVersion', value: '1' })
+  })
+}
+
